d817d710f0864496b2d42895da8d115fd04b9c0d
kpwd <jzmplusonesecond@outlook.com>
2018-11-24 23:01:31 +0800

    Update wangyi.c

--- wangyi.c
+++ wangyi.c
@@ -1,25 +1,39 @@
 //命令格式: ./可执行文件 输入.uc! 输出.mp3
+//./可执行文件 输入.uc!
+
 //在android gcc/manjaro linux gcc 编译通过
 #include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
 int main(int argc,char* arg[]){
     char temp_bytes[1024];
     unsigned int read_count,wrote_size=0;
-    if(argc<3){
+    if(argc<2){
         return 1;
     }
     FILE* i=fopen(arg[1],"r");
     FILE* o=fopen(arg[2],"w");
-    printf("InputFile:%s\r\n",arg[1]);
-    printf("OutputFile:%s\r\n",arg[2]);
-    if(!i || !o){
-        printf("Cannot open either file!\r\n");
+    if(!i){
+        printf("Cannot open file!\r\n");
         return 1;
     }
+    printf("InputFile:%s\r\n",arg[1]);
+    if(!o){
+    	const char *add=".mp3\n";
+    	char len=strlen(arg[1]);
+		char *n=(char *)malloc(len+4+1);
+		memcpy(n,arg[1],len);
+		memcpy(n+len,add,5);
+		printf("OutputFile:%s\r\n",n);
+    	o=fopen(n,"w");
+    	free(n);
+    }else
+    	printf("OutputFile:%s\r\n",arg[2]);
     
     while((read_count=fread(temp_bytes,sizeof(char),1024,i))>0){
         unsigned int p;
         wrote_size+=read_count;
-        printf("Cracking position:%u\r\n",wrote_size);
+        //printf("Cracking position:%u\r\n",wrote_size);
         for(p=0;p<read_count;p++){
             temp_bytes[p]^=0xa3;
         }
@@ -34,4 +48,5 @@ int main(int argc,char* arg[]){
     printf("Done!\r\n");
     fclose(i);
     fclose(o);
-}
+} 
+
